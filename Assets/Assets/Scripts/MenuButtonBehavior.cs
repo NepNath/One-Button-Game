15295a07dfaec24c9dc9d9202efc6a75bde995f3
Nathan.lemouleduparc <nathan.lemouleduparc@ynov.com>
2025-01-14 11:37:38 +0100

    adding test menu

---
+++ Assets/Assets/Scripts/MenuButtonBehavior.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtonBehavior : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void startGame()
+    {
+        // Load the game scene
+        SceneManager.LoadScene("MainGame");
+    }
+
+    public void quitGame()
+    {
+        // Quit the game
+        Application.Quit();
+    }
+}
